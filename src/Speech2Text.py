7cc275cbc5f77ed2372cc0b7169fe81f802fcbac
David Soller <soller.23@osu.edu>
2016-11-19 16:15:25 -0500

    Added Speech2Text v1.0
    - [x] Prompt for voice input
    - [x] Local version
    - [x] Google version (Uses default token)

--- src/Speech2Text.py
+++ src/Speech2Text.py
@@ -1,77 +1,51 @@
 #!/usr/bin/env python3
-
 # NOTE: this example requires PyAudio because it uses the Microphone class
 
 import speech_recognition as sr
+import time as time
+from datetime import datetime
 
-# obtain audio from the microphone
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Say something!")
-    audio = r.listen(source)
+TIME = False
 
-# recognize speech using Sphinx
-try:
-    print("Sphinx thinks you said " + r.recognize_sphinx(audio))
-except sr.UnknownValueError:
-    print("Sphinx could not understand audio")
-except sr.RequestError as e:
-    print("Sphinx error; {0}".format(e))
+class Speech2Text:
+    def __init__(self):
+        self.r = sr.Recognizer()
 
-# recognize speech using Google Speech Recognition
-try:
-    # for testing purposes, we're just using the default API key
-    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-    # instead of `r.recognize_google(audio)`
-    print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
-except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+    def listen(self):
+        # Provide count down until speaking
+        for i in range(3):
+            print(3-i)
+            time.sleep(1)
 
-# recognize speech using Wit.ai
-WIT_AI_KEY = "INSERT WIT.AI API KEY HERE" # Wit.ai keys are 32-character uppercase alphanumeric strings
-try:
-    print("Wit.ai thinks you said " + r.recognize_wit(audio, key=WIT_AI_KEY))
-except sr.UnknownValueError:
-    print("Wit.ai could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Wit.ai service; {0}".format(e))
+        # obtain audio from the microphone
+        with sr.Microphone() as source:
+            print("Say something!")
+            # audio = r.record(source, duration=2)
+            self.r.phrase_threshold = 2
+            audio = self.r.listen(source, timeout = 2)
+        
+        if TIME: start = datetime.now()
+        # self.__localSpeech2Text(audio)
+        self.__googleSpeech2Text(audio)
+        if TIME: end = datetime.now(); print("Time taken: {}".format(end-start))
+    
+    def __localSpeech2Text(self, audio):
+        # recognize speech using Sphinx
+        try:
+            print("Sphinx thinks you said: " + self.r.recognize_sphinx(audio))
+        except sr.UnknownValueError:
+            print("Sphinx could not understand audio")
+        except sr.RequestError as e:
+            print("Sphinx error; {0}".format(e))
 
-# recognize speech using Microsoft Bing Voice Recognition
-BING_KEY = "INSERT BING API KEY HERE" # Microsoft Bing Voice Recognition API keys 32-character lowercase hexadecimal strings
-try:
-    print("Microsoft Bing Voice Recognition thinks you said " + r.recognize_bing(audio, key=BING_KEY))
-except sr.UnknownValueError:
-    print("Microsoft Bing Voice Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Microsoft Bing Voice Recognition service; {0}".format(e))
-
-# recognize speech using api.ai
-API_AI_CLIENT_ACCESS_TOKEN = "INSERT API.AI API KEY HERE" # api.ai keys are 32-character lowercase hexadecimal strings
-try:
-    print("api.ai thinks you said " + r.recognize_api(audio, client_access_token=API_AI_CLIENT_ACCESS_TOKEN))
-except sr.UnknownValueError:
-    print("api.ai could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from api.ai service; {0}".format(e))
-
-# recognize speech using Houndify
-HOUNDIFY_CLIENT_ID = "INSERT HOUNDIFY CLIENT ID HERE" # Houndify client IDs are Base64-encoded strings
-HOUNDIFY_CLIENT_KEY = "INSERT HOUNDIFY CLIENT KEY HERE" # Houndify client keys are Base64-encoded strings
-try:
-    print("Houndify thinks you said " + r.recognize_houndify(audio, client_id=HOUNDIFY_CLIENT_ID, client_key=HOUNDIFY_CLIENT_KEY))
-except sr.UnknownValueError:
-    print("Houndify could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Houndify service; {0}".format(e))
-
-# recognize speech using IBM Speech to Text
-IBM_USERNAME = "INSERT IBM SPEECH TO TEXT USERNAME HERE" # IBM Speech to Text usernames are strings of the form XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
-IBM_PASSWORD = "INSERT IBM SPEECH TO TEXT PASSWORD HERE" # IBM Speech to Text passwords are mixed-case alphanumeric strings
-try:
-    print("IBM Speech to Text thinks you said " + r.recognize_ibm(audio, username=IBM_USERNAME, password=IBM_PASSWORD))
-except sr.UnknownValueError:
-    print("IBM Speech to Text could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from IBM Speech to Text service; {0}".format(e))
+    def __googleSpeech2Text(self, audio):
+        # recognize speech using Google Speech Recognition
+        try:
+            # for testing purposes, we're just using the default API key
+            # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+            # instead of `r.recognize_google(audio)`
+            print("Google Speech Recognition thinks you said: " + self.r.recognize_google(audio))
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
