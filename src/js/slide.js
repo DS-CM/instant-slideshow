590ab691170a831aa5945b172eb4505f7a7b1518
David Soller <soller.23@osu.edu>
2016-11-20 10:54:13 -0500

    Fixed Reveal js slideshow switching

--- src/js/slide.js
+++ src/js/slide.js
@@ -1,10 +1,19 @@
+var prevURL = "";
+
 (function updateImage() {
     $.ajax({
         url: "http://localhost:8080/imagelink",
         type: "GET",
         dataType: 'text',
         success: function(data) {
-            $('#slide').attr("src", data);
+            if (data !== prevURL) {
+                prevURL = data;
+                var slide = $( "<section class='present'><img></section>" );
+                $( slide ).find("img").attr("src", data);
+                $( '.present' ).removeClass( "present" ).addClass( "past" );
+                $( '#slides' ).append( slide );
+                $( '.navigate-right' ).prop('enabled', true); //just enable it, even if it is
+            }
         },
         // fail: function() {
         //     alert("error");
